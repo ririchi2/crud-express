44df08ce1280fb12409b24f38ad1c5142c429ef6
richipineda <richipineda2@gmail.com>
2021-11-11 19:21:34 +0000

    dice api

--- src/server.js
+++ src/server.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Successful response");
+const home = express.Router();
+const dice = express.Router();
+
+home.get("/", (req, res) => {
+    res.send("Homepage");
 });
+
+dice.get("/dice", (req, res) => {
+    const faces = 6;
+    const dice = Math.floor(faces * Math.random()) + 1;
+    res.send("Dice: " + dice);
+});
+
+dice.get("/dice/:id", (req, res) => {
+    const id = req.params.id;
+    const dice = Math.floor(id * Math.random()) + 1;
+    res.send("Dice: " + dice);
+});
+
+app.use(home);
+app.use(dice);
 
 app.listen(3001, () => console.log("Listening on port: 3001"));
